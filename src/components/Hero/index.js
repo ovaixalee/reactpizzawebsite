1b37a116a7b4997e0e06319687185e51b13afbea
Ovais Ali <ovaisali@wl-l2-70.rz.tu-ilmenau.de>
2021-06-09 16:53:18 +0200

    moving folders to components

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import { Navbar } from '../components/Navbar'
-import Sidebar from '../Sidebar'
+import { Navbar } from '../Navbar'
+import Sidebar from '../../Sidebar'
 import {HeroContainer, 
     HeroContent,
     HeroItems,
